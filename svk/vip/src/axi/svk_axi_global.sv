65a8c957715409d059b0995e3b3b48932d6ea2f5
Jakod <JakodYuan@outlook.com>
2023-09-11 14:44:39 +0800

    licence

--- svk/vip/src/axi/svk_axi_global.sv
+++ svk/vip/src/axi/svk_axi_global.sv
@@ -1,18 +1,8 @@
-/**
- *  Copyright (C) 2023-2024 JakodYuan. ( JakodYuan@outlook.com )
- *
- *  Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *          http://www.gnu.org/licenses/lgpl.html
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+/***********************************************************
+ *  Copyright (C) 2023 by JakodYuan (JakodYuan@outlook.com).
+ *  All right reserved.
+************************************************************/
+
 
 function void get_lanes(svk_axi_transaction tr, int beat, ref int lower_byte_lane, ref int upper_byte_lane, input int unsigned data_bus_bytes=0);
     automatic bit [`SVK_AXI_ADDR_WIDTH-1:0]  address_n;
